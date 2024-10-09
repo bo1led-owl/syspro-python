ea82ce41af4e6a1b7eb7386ead852556feb04d00
Ilya Sovtsov <ilyasovty@gmail.com>
2024-10-09 21:08:57 +0700

    Add new tests for mini 3

--- minis/3.py
+++ minis/3.py
@@ -9,8 +9,24 @@ def parseWierdMatrix(raw: str) -> [[float]]:
 
 
 class TestSuite(unittest.TestCase):
-    def test(self):
+    def testBasic(self):
         data = "1 3 2 | 2 1 3"
+        self.assertEqual(
+            [[1, 3, 2], [2, 1, 3]],
+            parseWierdMatrix(data),
+            f"input: {data}"
+        )
+
+    def testManyRows(self):
+        data = "1|2|3|4|5|6|7|8"
+        self.assertEqual(
+            [[1], [2], [3], [4], [5], [6], [7], [8]],
+            parseWierdMatrix(data),
+            f"input: {data}"
+        )
+
+    def testTooMuchWhitespace(self):
+        data = "  1  \t\n \r 3   2  \r\n|\t  \t2  \t\r\v 1   3 "
         self.assertEqual(
             [[1, 3, 2], [2, 1, 3]],
             parseWierdMatrix(data),
