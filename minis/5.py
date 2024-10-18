d576e44c66718d0b198a46f9bd8664e26c114ebb
Ilya Sovtsov <ilyasovty@gmail.com>
2024-10-18 21:49:20 +0700

    Make mini 5 solution return function with pretty `__name__` and `__doc__`

--- minis/5.py
+++ minis/5.py
@@ -1,9 +1,38 @@
 import unittest
+import inspect
 
 
 def specialize(func, *applied_args, **applied_kwargs):
     def wrapper(*args, **kwargs):
         return func(*applied_args, *args, **applied_kwargs, **kwargs)
+
+    args_names = inspect.getfullargspec(func).args
+    if len(applied_args) >= len(args_names):
+        raise TypeError(f"Too many arguments passed to {func.__name__}")
+
+    formatted_args = ""
+    formatted_kwargs = ""
+    for i, v in enumerate(applied_args):
+        formatted_args += '_' + f"{args_names[i]}_{v}"
+    for k, v in applied_kwargs.items():
+        formatted_kwargs += '_' + f"{k}_{v}"
+
+    wrapper.__name__ = f"partial_{func.__name__}" + \
+        formatted_args +\
+        formatted_kwargs
+
+    formatted_args = ""
+    formatted_kwargs = ""
+    for i, v in enumerate(applied_args):
+        formatted_args += ' ' + f"{args_names[i]}={v}"
+    for k, v in applied_kwargs.items():
+        formatted_kwargs += ' ' + f"{k}={v}"
+
+    wrapper.__doc__ = func.__doc__
+    if wrapper.__doc__ is not None and (applied_args or applied_kwargs):
+        trailing_dot = '.' if not func.__doc__.endswith('.') else ''
+        wrapper.__doc__ += trailing_dot +\
+            " Specialized with" + formatted_args + formatted_kwargs
     return wrapper
 
 
@@ -13,6 +42,7 @@ class Tests(unittest.TestCase):
             return x * y
 
         double = specialize(multiply, 2)
+
         self.assertEqual(double(10), 20)
         self.assertEqual(double(y=15), 30)
 
@@ -21,6 +51,7 @@ class Tests(unittest.TestCase):
             return x + y
 
         plus_one = specialize(sum, y=1)
+
         self.assertEqual(plus_one(10), 11)
         self.assertEqual(plus_one(x=4), 5)
         self.assertEqual(plus_one(x=-42), -41)
@@ -37,11 +68,48 @@ class Tests(unittest.TestCase):
 
     def testEverything(self):
         def foo(a, b, c, d=4):
+            "Foo. Does something"
             return (a + b + c) / d
 
         partial_foo = specialize(foo, 1, c=3)
+
+        self.assertEqual(partial_foo.__name__, "partial_foo_a_1_c_3")
+        self.assertEqual(partial_foo.__doc__,
+                         "Foo. Does something. Specialized with a=1 c=3")
+
         self.assertEqual(partial_foo(2), 1.5)
         self.assertEqual(partial_foo(2, d=2), 3)
+
+    def testTooManyArgs(self):
+        def sum(a, b):
+            return a + b
+
+        got_exception = False
+        try:
+            partial_sum = specialize(sum, 1, 2, 3)
+        except TypeError:
+            got_exception = True
+
+        self.assertTrue(
+            got_exception,
+            "Expected to get a `TypeError` for too many args"
+        )
+
+    def testNewKwarg(self):
+        def sum(a, b):
+            return a + b
+
+        partial_sum = specialize(sum, c=4)
+        got_exception = False
+        try:
+            partial_sum(1, 2)
+        except TypeError:
+            got_exception = True
+
+        self.assertTrue(
+            got_exception,
+            "Expected to get a `TypeError` for wrong kwarg"
+        )
 
 
 if __name__ == "__main__":
